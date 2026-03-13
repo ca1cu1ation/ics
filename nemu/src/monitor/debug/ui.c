ee079318958709c212a87b0885d52f0835872f90
tracer-ics2017 <tracer@njuics.org>
2026-03-13 19:56:30 +0800

    >  compile 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:56:28 up 7 min,  1 user,  load average: 0.25, 0.15, 0.08 70d88b8b3e02c1cf76b5be2846b13e9ef758479c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -1,6 +1,7 @@
 #include "monitor/monitor.h"
 #include "monitor/expr.h"
 #include "monitor/watchpoint.h"
+#include "cpu/reg.h"
 #include "nemu.h"
 
 #include <stdlib.h>
@@ -39,34 +40,34 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 static int cmd_info(char *args);
 
-static void reg_display(){
- int i;
- for(i=0;i<8;i++){
-  printf("%3s\t0x%08x\t%u\n",regsl[i],reg_l(i),reg_l(i));
- }
- printf("eip\t0x%8x\t%u\n",cpu.eip,cpu.eip);
+static void reg_display() {
+  int i;
+  for (i = 0; i < 8; i ++) {
+    printf("%3s\t0x%08x\t%u\n", regsl[i], reg_l(i), reg_l(i));
+  }
+  printf("eip\t0x%08x\t%u\n", cpu.eip, cpu.eip);
 }
 
-static int cmd_info(char *args){
- char *arg = strtok(args," ");
- if (arg == NULL) {
- printf("Usage: info r|w\n");
- return 0;
- }
+static int cmd_info(char *args) {
+  char *arg = strtok(args, " ");
+  if (arg == NULL) {
+    printf("Usage: info r|w\n");
+    return 0;
+  }
 
- if (strcmp(arg,"r") == 0){
- reg_display();
- return 0;
- }
+  if (strcmp(arg, "r") == 0) {
+    reg_display();
+    return 0;
+  }
 
- if (strcmp(arg,"w") ==0){
- wp_display();
- return 0;
- }
+  if (strcmp(arg, "w") == 0) {
+    wp_display();
+    return 0;
+  }
 
- printf("Unknown info subcommand '%s'\n",arg);
- printf("Usage: info r|w\n");
- return 0;
+  printf("Unknown info subcommand '%s'\n", arg);
+  printf("Usage: info r|w\n");
+  return 0;
 }
 
 static struct {
@@ -77,7 +78,8 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "info", "Print program status (info r: registers,info w:watchpoins)",cmd_info },
+  { "info", "Print program status (info r: registers, info w: watchpoints)", cmd_info },
+
   /* TODO: Add more commands */
 
 };
