02a6493424a028305502534d3c0471d04d1d3dc7
tracer-ics2017 <tracer@njuics.org>
2026-04-01 09:18:11 +0800

    >  compile 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:18:02 up 18 min,  1 user,  load average: 0.56, 0.34, 0.27 373e4194d9a53c3f87d98ae327fa0062f32510e0

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -2,21 +2,32 @@
 
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
+void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
 make_EHelper(lidt) {
-  TODO();
+  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
+  cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
 
   print_asm_template1(lidt);
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
+  switch (id_dest->reg) {
+    case 0: cpu.cr0.val = id_src->val; break;
+    case 3: cpu.cr3.val = id_src->val; break;
+    default: panic("unimplemented control register CR%d", id_dest->reg);
+  }
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+  switch (id_src->reg) {
+    case 0: rtl_li(&t0, cpu.cr0.val); break;
+    case 3: rtl_li(&t0, cpu.cr3.val); break;
+    default: panic("unimplemented control register CR%d", id_src->reg);
+  }
+  operand_write(id_dest, &t0);
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
@@ -26,7 +37,9 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  TODO();
+  raise_intr(id_dest->val, *eip);
+  decoding.jmp_eip = cpu.eip;
+  decoding.is_jmp = 1;
 
   print_asm("int %s", id_dest->str);
 
@@ -36,7 +49,10 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
+  rtl_pop(&decoding.jmp_eip);
+  rtl_pop(&t0);
+  cpu.eflags = t0;
+  decoding.is_jmp = 1;
 
   print_asm("iret");
 }
@@ -45,7 +61,8 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  TODO();
+  rtl_li(&t0, pio_read(id_src->val, id_dest->width));
+  operand_write(id_dest, &t0);
 
   print_asm_template2(in);
 
@@ -55,7 +72,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  TODO();
+  pio_write(id_dest->val, id_src->width, id_src->val);
 
   print_asm_template2(out);
 
