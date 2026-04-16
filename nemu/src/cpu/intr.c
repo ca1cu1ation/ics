66fefe6d0a131c4343ebf6ce9f52bd41e4cf02dd
tracer-ics2017 <tracer@njuics.org>
2026-04-16 14:37:24 +0800

    >  compile 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:37:15 up 41 min,  1 user,  load average: 0.50, 0.74, 0.56 56141791d144d2486ffba855d8466f1dd86595d6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,20 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  uint32_t gate_addr = cpu.idtr.base + NO * 8;
+  assert((NO * 8 + 7) <= cpu.idtr.limit);
 
-  TODO();
+  uint32_t low = vaddr_read(gate_addr, 4);
+  uint32_t high = vaddr_read(gate_addr + 4, 4);
+  vaddr_t handler = (high & 0xffff0000) | (low & 0x0000ffff);
+
+  /* iret in this framework pops eip first, then eflags. */
+  rtlreg_t temp = cpu.eflags;
+  rtl_push(&temp);
+  temp = ret_addr;
+  rtl_push(&temp);
+
+  cpu.eip = handler;
 }
 
 void dev_raise_intr() {
