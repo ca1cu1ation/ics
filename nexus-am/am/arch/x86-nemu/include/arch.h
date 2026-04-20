32a618f0ac0df34438645b07afff179fa2d0fe76
tracer-ics2017 <tracer@njuics.org>
2026-04-20 15:36:19 +0800

    >  run 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:36:11 up  1:29,  1 user,  load average: 0.69, 0.48, 0.30 70e229c51356161a2e2470bd1106bf1f475312f7

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -7,18 +7,15 @@
 #define PGSIZE    4096    // Bytes mapped by a page
 
 struct _RegSet {
-  uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
-  int       irq;
+  /* pushal saves registers in this order on stack: edi, esi, ebp, esp, ebx, edx, ecx, eax */
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uintptr_t irq, error_code, eip, cs, eflags;
 };
 
-/*
- * Trap frame layout in x86-nemu trap.S (after pushal):
- * [0] edi [1] esi [2] ebp [3] esp [4] ebx [5] edx [6] ecx [7] eax ...
- */
-#define SYSCALL_ARG1(r) ((uintptr_t)(((uint32_t *)(r))[7]))
-#define SYSCALL_ARG2(r) ((uintptr_t)(((uint32_t *)(r))[4]))
-#define SYSCALL_ARG3(r) ((uintptr_t)(((uint32_t *)(r))[6]))
-#define SYSCALL_ARG4(r) ((uintptr_t)(((uint32_t *)(r))[5]))
+#define SYSCALL_ARG1(r) ((r)->eax)
+#define SYSCALL_ARG2(r) ((r)->ebx)
+#define SYSCALL_ARG3(r) ((r)->ecx)
+#define SYSCALL_ARG4(r) ((r)->edx)
 
 #ifdef __cplusplus
 extern "C" {
