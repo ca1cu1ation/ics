eeefff668595b98df73fe8cb108d04fc8e5c6d24
2312819-Chen Yibin <2312819@mail.nankai.edu.cn>
2026-04-29 19:36:50 +0800

    >  run 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:36:46 up 47 min,  1 user,  load average: 0.46, 0.26, 0.20 aa53a5103e6adad565e0de39aad4a65e6ab87e5b

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,30 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  const uint32_t *pixels = (const uint32_t *)buf;
+  size_t pixel_offset = offset / sizeof(uint32_t);
+  size_t pixel_count = len / sizeof(uint32_t);
+
+  if (pixel_count == 0) {
+    return;
+  }
+
+  int x = pixel_offset % _screen.width;
+  int y = pixel_offset / _screen.width;
+
+  while (pixel_count > 0 && y < _screen.height) {
+    int row_space = _screen.width - x;
+    int row_pixels = (int)(pixel_count < (size_t)row_space ? pixel_count : (size_t)row_space);
+    _draw_rect(pixels, x, y, row_pixels, 1);
+    pixels += row_pixels;
+    pixel_count -= row_pixels;
+    x = 0;
+    y++;
+  }
 }
 
 void init_device() {
@@ -25,4 +46,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  snprintf(dispinfo, sizeof(dispinfo), "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
 }
