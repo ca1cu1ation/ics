4e00e33eaeb96792b6b6ce24339921e42fedda78
tracer-ics2017 <tracer@njuics.org>
2026-05-06 14:58:17 +0800

    >  run 2312819 root Linux LAPTOP-N6OUKIU2 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:58:16 up 28 min,  1 user,  load average: 0.00, 0.03, 0.01 7198ccdbb71dac0c95a518b9e1da5505ba89d3b

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(){
-  FILE *fp = fopen("/dev/events", "r");
+  int fd = open("/dev/events", O_RDONLY);
   volatile int j = 0;
   while(1){
     j ++;
     if (j == 1000000) {
       char buf[256];
-      char *p = buf, ch;
-      while((ch = fgetc(fp)) != -1){
-        *p ++ = ch;
-        if(ch == '\n') {
-          *p = '\0';
-          break;
-        }
+      int n = read(fd, buf, 255);
+      if (n > 0) {
+        buf[n] = '\0';
+        printf("receive event: %s", buf);
+      } else {
+        printf("read error or EOF, n=%d\n", n);
       }
-
-      printf("receive event: %s", buf);
       j = 0;
     }
   }
-
-  fclose(fp);
   return 0;
 }
-
